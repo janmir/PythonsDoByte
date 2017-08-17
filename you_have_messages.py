64ed027f1d3735252d9f9ed059d1bbc2ec940473
Jan Miranda <paulzu100@gmail.com>
2017-08-17 13:59:58 +0900

    Added feature list

--- you_have_messages.py
+++ you_have_messages.py
@@ -2,5 +2,9 @@
 # -*- coding: utf-8 -*-
 
 #Features
-#1. Accepts a dictionary of messaages
+#1. Accepts a dictionary of messages for initialization
 #   - Message_ID, Message
+#2. Can generate the ff: return formats
+#   - html, json, xml, text
+#3. Add helper functions for converting Dictionaries to json format
+#4. 
