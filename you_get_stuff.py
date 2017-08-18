74d8ba92d4f7e21a39dbb03f65b59f7b95388601
Jan Miranda <paulzu100@gmail.com>
2017-08-19 00:14:21 +0900

    Event handler update
    Added initial functions for request handler.

--- you_get_stuff.py
+++ you_get_stuff.py
@@ -3,11 +3,28 @@
 
 #Features
 #1. Handle all incomming data from request and issue errors if needed
-#2. Accept a list dictionary of parameters for checking request data, on initializations
+#2. Accept a list dictionary of parameters for checking request data, on initialization
 #   - Dictionary is in {"event_name", "required", "type"} pair
 #   - Check if all required events are present, issue an error if not.
 #3. Accept a event dictionary when checking, should be called in the first parts of the program
 #4. Do all format i.e number types should be a parsable number
 #5. Change dictionary to object attibutes
 #   - see https://goo.gl/WtXWe3
-#6.  
+
+import pytest
+
+class You_Get_Stuff:
+    def __init__(self, event):
+        self.event = event;
+
+    def you_check_it():
+        pass;
+    
+    def you_pack_it():
+        pass;
+
+    def you_parse_it():
+        pass;
+        
+def test_main():
+    assert True; 
