639d270fbe9888a5f244c0b1afb78965d6ed8e62
Jan Miranda <paulzu100@gmail.com>
2017-08-20 16:36:30 +0900

    You get stuff fuction implementations
    Implemented all functions within you_get_stuff module.

--- you_get_stuff.py
+++ you_get_stuff.py
@@ -13,18 +13,87 @@
 
 import pytest
 
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
 class You_Get_Stuff:
+    stuff = {};
+    valid = False;
+
     def __init__(self, event):
         self.event = event;
 
-    def you_check_it():
-        pass;
+    def you_check_it(self, request):
+        result = True;
+        #iterate thru all values of the json dictionary
+        for event in self.event:
+            #convet the json to something like this -> name.key
+            struct = Struct(**event);
+            #check required
+            if(struct.required.lower() in ("yes", "true", "1")):
+                if(struct.name not in request):
+                    print(struct.name + " not it request. Throw an error.");
+                    result &= False;
+            
+            #check data type
+            if(struct.type not in ("int", "float", "string", "bool")):
+                print(struct.name + "datatype in request is invalid. Throw an error.");
+                result &= False;
+                
+        self.valid = result;
+        return result;
     
-    def you_pack_it():
-        pass;
+    def you_pack_it(self):
+        return Struct(**self.stuff);
 
-    def you_parse_it():
-        pass;
+    def you_parse_it(self):
+        #if precheck yeilds valid
+        if(self.valid):
+            print("request is valid... parsing now.");
+
+            #check type of item then convert it
+            for event in self.event:
+                struct = Struct(**event);
+
+                value = None;
+
+                #Parse the value
+                try:
+                    if(struct.type == "int"):
+                        value = int(struct.value);
+                    elif(struct.type == "float"):
+                        value = float(struct.value);
+                    elif(struct.type == "bool"):
+                        value = bool(struct.value);
+                    elif(struct.type == "string"):
+                        value = struct.value;
+                except Exception as e:
+                    print("Error: " + e.message);
+                
+                #Assign value
+                self.stuff[struct.name] = value;
+
+            print(self.stuff.keys());
+        else:
+            print("request not yet checked or invalid.")
+
         
 def test_main():
-    assert True; 
+    testEvent = [
+                    {"name": "event1", "value":"1", "required": "true", "type": "string"},
+                    {"name": "event2", "value":"2", "required": "true", "type": "bool"},
+                    {"name": "event3", "value":"3", "required": "true", "type": "int"},
+                    {"name": "event4", "value":"4", "required": "true", "type": "float"}
+    ];
+    testEventRequest ={"event1": "value herer", "event2": "value herer", "event3": "value herer", "event4": "value herer"}
+
+    testClass = You_Get_Stuff(testEvent);
+    testClass.you_check_it(testEventRequest);
+    testClass.you_parse_it();
+    assert testClass.you_pack_it().event1 == "1";
+    assert testClass.you_pack_it().event2 == True;
+    assert testClass.you_pack_it().event3 == 3;
+    assert testClass.you_pack_it().event4 == 4.0;
+
+#test_main();
